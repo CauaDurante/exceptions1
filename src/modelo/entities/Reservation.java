35b136c3460f5a8d93a3fa64c7a8b724ff2d63aa
Cauã Durante Lopes de Oliveira <caua.oliveira@pmpa.ad>
2025-08-01 16:17:52 -0300

    Very bad solution

---
+++ src/modelo/entities/Reservation.java
@@ -0,0 +1,59 @@
+package modelo.entities;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private LocalDate chekIn;
+    private LocalDate checkOut;
+
+    private static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Reservation(Integer roomNumber, LocalDate chekIn, LocalDate checkOut) {
+        this.roomNumber = roomNumber;
+        this.chekIn = chekIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public LocalDate getChekIn() {
+        return chekIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration(){
+        Duration d1 = Duration.between(chekIn.atTime(0, 0), checkOut.atTime(0, 0));
+        return d1.toDays();
+    }
+
+    public void updateDates(LocalDate checkIn, LocalDate checkOut){
+        this.chekIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString(){
+        return "Room "
+                + roomNumber
+                + ", check-in: "
+                + fmt.format(chekIn)
+                + ", check-out: "
+                + fmt.format(checkOut)
+                + ", "
+                + duration()
+                + " nights";
+    }
+}
